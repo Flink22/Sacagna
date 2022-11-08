fa38622330e8e19a1f4f1e0140672c5518a6158c
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2022-11-08 12:15:14 +0100

    Add files via upload

--- Raspberry/Laser.py
+++ Raspberry/Laser.py
@@ -6,7 +6,7 @@ import adafruit_vl6180x as vl6180
 
 laser_SHDN = [5, 6, 13, 19, 26]
 laser_ADDRESS = [0x20, 0x22, 0x24, 0x26, 0x28]
-laser_MM = [5, 6, 13, 19, 26]
+laser_MM = [0, 0, 0, 0, 0]
 i2c = busio.I2C(board.SCL, board.SDA)
 
 def init():
@@ -19,20 +19,14 @@ def init():
         gpio.output(laser_SHDN[i],gpio.HIGH)
         sensor = vl6180.VL6180X(i2c)
         sensor._write_8(0x212, laser_ADDRESS[i])
-        time.sleep(1)
+        time.sleep(0.5)
     
 
 def read():
     
     for i in range(0, 5):
         sensor = vl6180.VL6180X(i2c, address = laser_ADDRESS[i])
-        laser_MM[i] = sensor.range
+        laser_MM[i] = sensor.range + 10
         print("Range: {0}mm".format(laser_MM[i]))
         
     time.sleep(1)
-
-if __name__ == "__main__":
-    init()
-    while True:
-        read()
-        print(" ------------- ")
