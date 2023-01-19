bccfb4b685be62f412fefdcb46a9663c58680b3b
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-01-19 15:18:34 +0100

    Update laser.py

--- Raspberry/laser.py
+++ Raspberry/laser.py
@@ -5,11 +5,15 @@ import time
 import adafruit_vl6180x as vl6180
 
 laser_SHDN = [23, 24, 20, 21, 18]
+bno_SHDN = 9
 laser_ADDRESS = [0x20, 0x22, 0x24, 0x26, 0x28]
 laser_MM = [0, 0, 0, 0, 0]
 i2c = busio.I2C(board.SCL, board.SDA)
 
 def init():
+    
+    gpio.setup(bno_SHDN,gpio.OUT)
+    gpio.output(bno_SHDN,gpio.LOW) #ricordati di accendere il bno!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     
     for i in range(0, 5):
         gpio.setup(laser_SHDN[i],gpio.OUT)
