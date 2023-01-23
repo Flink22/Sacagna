c6582df130a4b2c06c78c172af92d32c91e51182
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-01-23 08:58:39 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -4,17 +4,32 @@
 #include <math.h>
 
 #define n_mot 4
+#define kp 150
+#define ki 5
+#define kd 7
 
 volatile int mot_cursor;
+
+volatile int pid_mot;
+volatile float speed_error;
+volatile float wanted_speed;
+volatile float integral[n_mot];
+volatile float derivative;
+volatile long int old_error[n_mot];
+volatile int correction;
+
 volatile long int mot_curr[n_mot];
 volatile long int mot_old[n_mot];
 volatile long int mot_temp[n_mot];
 volatile int mot_dir[n_mot];
-volatile int pid_mot;
+
 volatile float dist_travelled;
 volatile float dist_temp;
 volatile float speed;
 
+volatile int driver[n_mot][4];
+
+volatile int i;
 
 void init_gpio();
 void init_intt();
@@ -22,7 +37,7 @@ void init_timer4();
 void init_pwm();
 void start_pwm();
 void init_pid();
-void pid();
+void driver_set();
 
 int main(void){
 
@@ -32,219 +47,272 @@ int main(void){
 	init_pwm();
 	init_pid();
 	
-	int rpm_destra_A=0, rpm_sinistra_B=0;
+	sei();
 	
-	while(1) {
-		
-		rpm_destra_A = 2000000/(mot_curr[0] * 990) * 60;
-		rpm_sinistra_B = 2000000/(mot_curr[1] * 990) * 60;
-		
-		if(rpm_destra_A> 15){
-			PORTA= 0x01;
-		} else{
-			PORTA=0x00;
-		}
-		if(rpm_sinistra_B> 15){
-			PORTA= 0x02;
-			} else{
-			PORTA=0x00;
-		}
-		
-		
-		
+	for(i=0;i<4;i++){
+		integral[i] = 0;
 	}
+	
+	start_pwm();
+
+	while(1) {
+		driver_set();
+	}
+
 }
 
 void init_gpio(){ //disattivo interrupt esterni e definisco entrate ed uscite
-	
+
 	cli();
+
+	DDRA = 0xF8;
+	DDRJ = 0x8F;
 	
-	DDRA = 0xFF;
-	DDRB = 0xF0;
-	DDRC = 0xFF;	
+	DDRD = 0x0F;
+	DDRH = 0x0F;
 	
+	DDRL = 0x08;
+	
+	DDRE = 0x1A;
+	DDRB = 0x68;
+
 }
 
 void init_intt(){ //definisco gli interrupt esterni e li attivo
-	
+
 	EICRA = 0xFF;
 	EIMSK = (1<<INT0)|(1<<INT1)|(1<<INT2)|(1<<INT3);
+
+}
+
+void init_timer4(){ //faccio partire il timer 4 in normal mode (presc. /8) per velocità motori
+
+	TCCR4A |= 0x00;
+	TCCR4B |= (1<<CS41);
+	TCCR4C |= 0x00;
+
+	TCNT4 = 0;
+
+}
+
+void init_pwm(){ //pwm 10 bit phase corrected con t/c1 e t/c3
+
+	TCCR1A |= (1<<WGM10)|(1<<WGM11);
+	TCCR1B |= (0<<WGM12);
+	TCCR1A |= (0<<COM1A0)|(1<<COM1A1);
+	TCCR1A |= (0<<COM1B0)|(1<<COM1B1);
+
+	TCCR3A |= (1<<WGM30)|(1<<WGM31);
+	TCCR3B |= (0<<WGM32);
+	TCCR3A |= (0<<COM3A0)|(1<<COM3A1);
+	TCCR3A |= (0<<COM3B0)|(1<<COM3B1);
+
+}
+
+void start_pwm(){ //prescaler /1 per t/c1 e t/c3 + enable driver
+
+	PORTA |= 0x20;
+	PORTJ |= 0x04;
+
+	TCCR1B |= (0<<CS12)|(0<<CS11)|(1<<CS10);
+	TCCR3B |= (0<<CS32)|(0<<CS31)|(1<<CS30);
+
+}
+
+void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (100 Hz)
+
+	TCCR0A = (1<<COM0A0)|(0<<COM0A1);
+	TCCR0B = (1<<CS02)|(0<<CS01)|(1<<CS00);
+	TIMSK0 = (1<<OCIE0A);
+	
+	OCR0A = 156;
+	
 	sei();
 	
 }
 
-void init_timer4(){ //faccio partire il timer 4 in normal mode (presc. /8) per velocità motori
+void driver_set(){
 	
-	TCCR4A = 0x00;
-	TCCR4B = (1<<CS41);
-	TCCR4C = 0x00;
-	
-	TCNT4 = 0;
-	
-}
-
-void init_pwm(){ //pwm 10 bit phase corrected con t/c1 e t/c3
-	
-	TCCR1A |= (1<<WGM10)|(1<<WGM11);
-	TCCR1A |= (0<<COM1A0)|(1<<COM1A1);
-	TCCR1A |= (0<<COM1B0)|(1<<COM1B1);
-	
-	TCCR3A |= (1<<WGM30)|(1<<WGM31);
-	TCCR3A |= (0<<COM3A0)|(1<<COM3A1);
-	TCCR3A |= (0<<COM3B0)|(1<<COM3B1);
-	
-}
-
-void start_pwm(){ //prescaler /64 per t/c1 e t/c3 + enable driver
-	
-	PORTC |= 0xC0;
-	
-	TCCR1B |= (0<<CS12)|(1<<CS11)|(1<<CS10);
-	TCCR3B |= (0<<CS32)|(1<<CS31)|(1<<CS30); 
-	
-}
-
-void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo ()
-	
-	TCCR0A = (1<<COM0A1)|(1<<COM0A0);
-	TCCR0B = (1<<CS00)|(0<<CS01)|(0<<CS02); //prescaler /256
-	
-}
-
-void pid(){
-	
-	if(pid_mot < 4){
-		pid_mot = pid_mot + 1;
-	} else{
-		pid_mot = 0;
-	}
-
-	speed = 0;
-	speed = 2000000/(mot_curr[pid_mot] * 990);
-
-	if (speed > 0.1){
+	switch(pid_mot){
 		
-		dist_temp = (70 * M_PI) * speed * 0.032768;
+		case 0:
+			if(driver[0][0]<0){
+				driver[0][0] = driver[0][0] * -1;
+				PORTJ &= 0b111;
+				PORTJ |= 0b10000;
+			}else{
+				PORTJ &= 0b111;
+				PORTJ |= 0b1000;
+			}
+			OCR1B = driver[0][0];
+			break;
+			
+		case 1:
+		if(driver[1][0]<0){
+			driver[1][0] = driver[1][0] * -1;
+			PORTJ &= 0b11100;
+			PORTJ |= 0b1;
+			}else{
+			PORTJ &= 0b11100;
+			PORTJ |= 0b10;
+		}
+		OCR1A = driver[1][0];
+		break;
+		
+		case 2:
+		if(driver[2][0]<0){
+			driver[2][0] = driver[2][0] * -1;
+			PORTA &= 0b11100000;
+			PORTA |= 0b1000;
+			}else{
+			PORTA &= 0b11100000;
+			PORTA |= 0b10000;
+		}
+		OCR3B = driver[2][0];
+		break;
+		
+		case 3:
+		if(driver[3][0]<0){
+			driver[3][0] = driver[3][0] * -1;
+			PORTA &= 0b111000;
+			PORTA |= 0b10000000;
+			}else{
+			PORTA &= 0b111000;
+			PORTA |= 0b1000000;
+		}
+		OCR3A = driver[3][0];
+		break;
 		
 	}
-	
-	error = wanted_speed - speed;
-	proportional = error * Kp;
-	integral[pid_mot] += Kint * error;
-	derivative = (error - olderror[pid_mot]) * Kdt;
-	olderror[pid_mot] = error;
-	
-	
-	
-	
-	
-	integrale[pid_mot] = integrale[M_at] > limI ? limI : integrale[M_at] < -limI ? -limI : integrale[M_at];
-	proporzionale = proporzionale > limP ? limP : proporzionale < -limP ? -limP : proporzionale;
-	derivata = derivata > limD ? limD : derivata < -limD ? -limD : derivata;
-	
-	correzione = proporzionale + derivata + integrale[M_at] + attuali[M_at]; 
-	correzione = correzione > 1023 ? 1023 : correzione < -1023 ? -1023 : correzione; 
 	
 }
 
 ISR(INT0_vect){
-	
+
 	mot_cursor = 0;
 	mot_temp[mot_cursor] = TCNT4;
-	
-	if((PORTB&0x01)!=0){
+
+	if((PINH&0x08)!=0){
 		mot_dir[mot_cursor] = 1;
-	} else{
+		} else{
 		mot_dir[mot_cursor] = -1;
 	}
-	
+
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	if(mot_temp[mot_cursor] < mot_old[mot_cursor]){
 		mot_old[mot_cursor] = mot_old[mot_cursor] - 65535;
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	mot_old[mot_cursor] = mot_temp[mot_cursor];
-	
+
 }
 
 ISR(INT1_vect){
-	
+
 	mot_cursor = 1;
 	mot_temp[mot_cursor] = TCNT4;
-	
-	if((PORTB&0x02)!=0){
+
+	if((PINH&0x04)!=0){
 		mot_dir[mot_cursor] = 1;
 		} else{
 		mot_dir[mot_cursor] = -1;
 	}
-	
+
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	if(mot_temp[mot_cursor] < mot_old[mot_cursor]){
 		mot_old[mot_cursor] = mot_old[mot_cursor] - 65535;
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	mot_old[mot_cursor] = mot_temp[mot_cursor];
-	
+
 }
 
 ISR(INT2_vect){
-	
+
 	mot_cursor = 2;
 	mot_temp[mot_cursor] = TCNT4;
-	
-	if((PORTB&0x04)!=0){
+
+	if((PINH&0x02)!=0){
 		mot_dir[mot_cursor] = 1;
 		} else{
 		mot_dir[mot_cursor] = -1;
 	}
-	
+
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	if(mot_temp[mot_cursor] < mot_old[mot_cursor]){
 		mot_old[mot_cursor] = mot_old[mot_cursor] - 65535;
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	mot_old[mot_cursor] = mot_temp[mot_cursor];
-	
+
 }
 
 ISR(INT3_vect){
-	
+
 	mot_cursor = 3;
 	mot_temp[mot_cursor] = TCNT4;
-	
-	if((PORTB&0x08)!=0){
+
+	if((PINH&0x01)!=0){
 		mot_dir[mot_cursor] = 1;
 		} else{
 		mot_dir[mot_cursor] = -1;
 	}
-	
+
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	if(mot_temp[mot_cursor] < mot_old[mot_cursor]){
 		mot_old[mot_cursor] = mot_old[mot_cursor] - 65535;
 		mot_curr[mot_cursor] = mot_temp[mot_cursor] - mot_old[mot_cursor];
 	}
-	
+
 	mot_old[mot_cursor] = mot_temp[mot_cursor];
-	
+
 }
 
 ISR(TIMER0_COMPA_vect){
+
+	if(pid_mot < 4){
+		pid_mot = pid_mot + 1;
+		} else{
+		pid_mot = 0;
+	}
+
+	speed = 0;
+	speed = 1000000/(mot_curr[pid_mot] * 990) * mot_dir[pid_mot];
+
+	if (speed > 0.1){
+
+		dist_temp = (70 * M_PI) * speed * 0.032768;
+
+	}
+
+	wanted_speed = 0.5;
+
+	speed_error = wanted_speed - speed;
+	integral[pid_mot] += speed_error * 0.039936;
+	derivative = (speed_error - old_error[pid_mot]) / 0.039936;
+	old_error[pid_mot] = speed_error;
+
+	correction = (speed_error * kp) + (derivative * kd) + (integral[pid_mot] * ki);
+	if(correction > 1023)
+	correction = 1023;
+	if(correction < 1023)
+	correction = -1023;
 	
-	pid();
+	driver[pid_mot][0] = correction;
 	
 }
