02dd38d4cd64dd9729faef5e45eda665ce1b6a05
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-02-04 21:22:27 +0100

    Update main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -2,21 +2,22 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <math.h>
+#include <util/delay.h>
 
 #define n_mot 4
-#define kp 150
-#define ki 5
-#define kd 7
+#define kp 2.5
+#define ki 0.1
+#define kd 0.1
 
 volatile int mot_cursor;
 
 volatile int pid_mot;
 volatile float speed_error;
-volatile float wanted_speed;
+volatile float wanted_speed[n_mot];
 volatile float integral[n_mot];
 volatile float derivative;
 volatile long int old_error[n_mot];
-volatile int correction;
+volatile long int correction;
 
 volatile long int mot_curr[n_mot];
 volatile long int mot_old[n_mot];
@@ -25,7 +26,8 @@ volatile int mot_dir[n_mot];
 
 volatile float dist_travelled;
 volatile float dist_temp;
-volatile float speed;
+volatile float speedf[n_mot];
+volatile long int speed[n_mot];
 
 volatile int driver[n_mot][4];
 
@@ -37,7 +39,39 @@ void init_timer4();
 void init_pwm();
 void start_pwm();
 void init_pid();
-void driver_set();
+void init_serial();
+
+unsigned char a = 0;
+unsigned char temp[3];
+
+void Serial_Tx(int data) {
+	
+	if (data>99) {
+		for (int k=0; k<3; k++){
+			temp[k] = data%10;
+			data = data / 10;
+		}
+	}else if(data>9) {
+		for (int k=0; k<2; k++){
+			temp[k] = data%10;
+			data = data / 10;
+		}
+		temp[2] = 0;
+	}else {
+		temp[0] = data;
+		temp[1] = 0;
+		temp[2] = 0;
+	}
+	
+	for (int k=0; k<3; k++){
+		while ( !( UCSR0A & (1<<UDRE0)) );
+		UDR0 = temp[k];
+		_delay_us(1);
+	}
+	
+	_delay_ms(1);
+	
+}
 
 int main(void){
 
@@ -46,6 +80,7 @@ int main(void){
 	init_timer4();
 	init_pwm();
 	init_pid();
+	init_serial();
 	
 	sei();
 	
@@ -54,9 +89,49 @@ int main(void){
 	}
 	
 	start_pwm();
-
+	
 	while(1) {
-		driver_set();
+		
+		if(driver[0][0]<0){
+			PORTJ &= 0b111;
+			PORTJ |= 0b10000;
+			OCR1B = (driver[0][0] * -1);
+			}else{
+			PORTJ &= 0b111;
+			PORTJ |= 0b1000;
+			OCR1B = driver[0][0];
+		}
+		
+		if(driver[1][0]<0){
+			PORTJ &= 0b11100;
+			PORTJ |= 0b1;
+			OCR1A = (driver[1][0] * -1);
+			}else{
+			PORTJ &= 0b11100;
+			PORTJ |= 0b10;
+			OCR1A = driver[1][0];
+		}
+		
+		if(driver[2][0]<0){
+			PORTA &= 0b11100000;
+			PORTA |= 0b01000;
+			OCR3B = (driver[2][0] * -1);
+			}else{
+			PORTA &= 0b11100000;
+			PORTA |= 0b10000;
+			OCR3B = driver[2][0];
+		}
+		
+		if(driver[3][0]<0){
+			PORTA &= 0b111000;
+			PORTA |= 0b10000000;
+			OCR3A = (driver[3][0] * -1);
+			}else{
+			PORTA &= 0b111000;
+			PORTA |= 0b01000000;
+			OCR3A = driver[3][0];
+		}
+	
 	}
 
 }
@@ -68,14 +143,14 @@ void init_gpio(){ //disattivo interrupt esterni e definisco entrate ed uscite
 	DDRA = 0xF8;
 	DDRJ = 0x8F;
 	
-	DDRD = 0x0F;
-	DDRH = 0x0F;
+	DDRD = 0x00;
+	DDRH = 0x00;
 	
 	DDRL = 0x08;
 	
 	DDRE = 0x1A;
 	DDRB = 0x68;
-
+	
 }
 
 void init_intt(){ //definisco gli interrupt esterni e li attivo
@@ -119,71 +194,25 @@ void start_pwm(){ //prescaler /1 per t/c1 e t/c3 + enable driver
 
 }
 
-void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (100 Hz)
+void init_serial(){
+	
+	UBRR0H = 0;
+	UBRR0L = 0;
+	UCSR0A = 0x00;
+	UCSR0B = 0x08;
+	UCSR0C = 0x06;
+	
+}
+
+void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (400 Hz, quindi 100 Hz a motore)
 
 	TCCR0A = (1<<COM0A0)|(0<<COM0A1);
-	TCCR0B = (1<<CS02)|(0<<CS01)|(1<<CS00);
+	TCCR0B = (1<<CS02)|(0<<CS01)|(0<<CS00);
 	TIMSK0 = (1<<OCIE0A);
 	
 	OCR0A = 156;
 	
 	sei();
-	
-}
-
-void driver_set(){
-	
-	switch(pid_mot){
-		
-		case 0:
-			if(driver[0][0]<0){
-				PORTJ &= 0b111;
-				PORTJ |= 0b10000;
-				OCR1B = driver[0][0] * -1;
-			}else{
-				PORTJ &= 0b111;
-				PORTJ |= 0b1000;
-				OCR1B = driver[0][0];
-			}
-			break;
-			
-		case 1:
-		if(driver[1][0]<0){
-			PORTJ &= 0b11100;
-			PORTJ |= 0b1;
-			OCR1A = driver[1][0] * -1;
-			}else{
-			PORTJ &= 0b11100;
-			PORTJ |= 0b10;
-			OCR1A = driver[1][0];
-		}
-		break;
-		
-		case 2:
-		if(driver[2][0]<0){
-			PORTA &= 0b11100000;
-			PORTA |= 0b1000;
-			OCR3B = driver[2][0] * -1;
-			}else{
-			PORTA &= 0b11100000;
-			PORTA |= 0b10000;
-			OCR3B = driver[2][0];
-		}
-		break;
-		
-		case 3:
-		if(driver[3][0]<0){
-			PORTA &= 0b111000;
-			PORTA |= 0b10000000;
-			OCR3A = driver[3][0] * -1;
-			}else{
-			PORTA &= 0b111000;
-			PORTA |= 0b1000000;
-			OCR3A = driver[3][0];
-		}
-		break;
-		
-	}
 	
 }
 
@@ -193,9 +222,9 @@ ISR(INT0_vect){
 	mot_temp[mot_cursor] = TCNT4;
 
 	if((PINH&0x08)!=0){
-		mot_dir[mot_cursor] = 1;
-		} else{
 		mot_dir[mot_cursor] = -1;
+		} else{
+		mot_dir[mot_cursor] = 1;
 	}
 
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
@@ -217,9 +246,9 @@ ISR(INT1_vect){
 	mot_temp[mot_cursor] = TCNT4;
 
 	if((PINH&0x04)!=0){
-		mot_dir[mot_cursor] = 1;
-		} else{
 		mot_dir[mot_cursor] = -1;
+		} else{
+		mot_dir[mot_cursor] = 1;
 	}
 
 	if(mot_temp[mot_cursor] > mot_old[mot_cursor]){
@@ -286,28 +315,28 @@ ISR(INT3_vect){
 ISR(TIMER0_COMPA_vect){
 
 	if(pid_mot < 4){
-		pid_mot = pid_mot + 1;
+		pid_mot += 1;
 		} else{
 		pid_mot = 0;
 	}
 
-	speed = 0;
-	speed = 1000000/(mot_curr[pid_mot] * 990) * mot_dir[pid_mot];
-
-	if (speed > 0.1){
-
-		dist_temp = (70 * M_PI) * speed * 0.032768;
-
+	speedf[pid_mot] = (((double)(2000000)) * 60)/(((double)mot_curr[pid_mot]) * 990);
+	if (speedf[pid_mot] < 10){
+		speedf[pid_mot] = 0;
 	}
+	speedf[pid_mot] *= mot_dir[pid_mot];
+	
+	speed[pid_mot] = speedf[pid_mot];
+	Serial_Tx(speed[pid_mot]);
 
-	wanted_speed = 0.5;
+	wanted_speed[pid_mot] = 100;
 
-	speed_error = wanted_speed - speed;
-	integral[pid_mot] += speed_error * 0.039936;
-	derivative = (speed_error - old_error[pid_mot]) / 0.039936;
+	speed_error = wanted_speed[pid_mot] - speed[pid_mot];
+	integral[pid_mot] += speed_error * 0.01;
+	derivative = (speed_error - old_error[pid_mot]) / 0.01;
 	old_error[pid_mot] = speed_error;
 
-	correction = (speed_error * kp) + (derivative * kd) + (integral[pid_mot] * ki);
+	correction = (speed_error * kp) + (derivative * kd) + (integral[pid_mot] * ki) + speed[pid_mot];
 	if(correction > 1023)
 	correction = 1023;
 	if(correction < 1023)
