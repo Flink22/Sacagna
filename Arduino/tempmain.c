2eb315ec93e7312d042139758fae245c1908cbb3
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-02-16 20:25:41 +0100

    Update tempmain.c

--- Arduino/tempmain.c
+++ Arduino/tempmain.c
@@ -6,118 +6,72 @@
 
 #define n_mot 4
 
-#define kp 150.0
+#define kp 140.0
 #define ki 2.0
-#define kd 7.0
+#define kd 50.0
+#define diametro 69.0
 
-int mot_cursor = 0;
-int pid_mot = 0;
+#define Plim 500
+#define Ilim 2
+#define Dlim 200
+
+double speed[n_mot] = {0};
+double speed_error = {0};
+double old_error[n_mot] = {0};
+double wanted_speed[n_mot] = {0};
+double setspeed;
+
+double dist_travelled;
+double dist_temp;
+
+int setdrv = 1;
 
 typedef struct {
-	unsigned int curr;
-	unsigned int old;
-	unsigned int temp;
+	double trav[4];
+	double temp;
+} def_dis;
+
+typedef struct {
+	int mot;
+	double P;
+	double I[n_mot];
+	double D;
+	unsigned int correction;
+} def_pid;
+
+typedef struct {
+	unsigned long int curr;
+	unsigned long int old;
+	unsigned long int temp;
 	int dir;
 } def_readspeed;
 
 typedef struct {
-	double speed;
-	double err;
-	double old_err;
-	double wanted;
-	double travelled;
-} def_speed;
-
-typedef struct {
+	char dir;
 	unsigned int pwm;
-	int dir;
 } def_driver;
 
-typedef struct {
-	double I;
-	double D;
-	double P;
-	int cor;
-} def_pid;
-
-typedef struct {
-	def_readspeed rs;
-	def_speed sp;
-	def_driver dr;
-	def_pid pid;
-} def_mott;
-
-typedef struct {
-	def_mott M[n_mot];
-} def_mot;
-
-
-int i;
-
-void init_gpio();
-void init_intt();
-void init_timer4();
-void init_pwm();
-void start_pwm();
-void init_pid();
-void init_serial();
-void driver_set();
-
-unsigned char a = 0;
-unsigned char temp[3];
+def_dis DIS;
+def_pid PiD;
+def_readspeed RSP[4];
+def_driver DVR[4];
 
 void Serial_Tx(int data) {
-
-	if(data < 0)
-	data *= -1;
 	
-	if (data>99) {
-		for (int k=0; k<3; k++){
-			temp[k] = data%10;
-			data = data / 10;
-		}
-		}else if(data>9) {
-		for (int k=0; k<2; k++){
-			temp[k] = data%10;
-			data = data / 10;
-		}
-		temp[2] = 0;
-		}else {
-		temp[0] = data;
-		temp[1] = 0;
-		temp[2] = 0;
+	unsigned char temp[4];
+	
+	for (int k=0; k<4; k++){
+		temp[k] = data%10;
+		data = data / 10;
 	}
 	
-	for (int k=0; k<3; k++){
+	for (int k=0; k<4; k++){
 		while ( !( UCSR0A & (1<<UDRE0)) );
 		UDR0 = temp[k];
 	}
 	
 	_delay_ms(1);
 	
-}
-
-int main(void){
-	
-	def_mot MOT;
-	init_gpio();
-	init_intt();
-	init_timer4();
-	init_pwm();
-	init_pid();
-	init_serial();
-	
-	sei();
-	
-	for(i=0;i<4;i++){
-		MOT.M[i].pid.I = 0;
-	}
-	
-	start_pwm();
-	
-	while(1) {
-	}
-
 }
 
 void init_gpio(){ //disattivo interrupt esterni e definisco entrate ed uscite
@@ -141,20 +95,25 @@ void init_intt(){ //definisco gli interrupt esterni e li attivo
 
 	EICRA = 0xFF;
 	EIMSK = (1<<INT0)|(1<<INT1)|(1<<INT2)|(1<<INT3);
+	
+	for(int i=0;i<4;i++){
+		RSP[i].curr = 65535;
+		RSP[i].old = 65535;
+	}
 
 }
 
 void init_timer4(){ //faccio partire il timer 4 in normal mode (presc. /8) per velocità motori
 
 	TCCR4A |= 0x00;
-	TCCR4B |= (1<<CS41)|(1<<CS40);
+	TCCR4B |= (1<<CS41)|(0<<CS40);
 	TCCR4C |= 0x00;
 
 	TCNT4 = 0;
 
 }
 
-void init_pwm(){ //pwm 10 bit phase corrected con t/c1 e t/c3
+void init_pwm(){ //pwm 10 bit con t/c1 e t/c3
 
 	TCCR1A |= (1<<WGM10)|(1<<WGM11);
 	TCCR1B |= (0<<WGM12);
@@ -173,8 +132,8 @@ void start_pwm(){ //prescaler /1 per t/c1 e t/c3 + enable driver
 	PORTA |= 0x20;
 	PORTJ |= 0x04;
 
-	TCCR1B |= (0<<CS12)|(0<<CS11)|(1<<CS10);
-	TCCR3B |= (0<<CS32)|(0<<CS31)|(1<<CS30);
+	TCCR1B |= (0<<CS12)|(1<<CS11)|(1<<CS10);
+	TCCR3B |= (0<<CS32)|(1<<CS31)|(1<<CS30);
 
 }
 
@@ -188,13 +147,13 @@ void init_serial(){
 	
 }
 
-void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (400 Hz, quindi 100 Hz a motore)
+void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (4000 Hz, quindi 1000 Hz a motore)
 
 	TCCR0A = (1<<COM0A0)|(0<<COM0A1);
-	TCCR0B = (1<<CS02)|(0<<CS01)|(0<<CS00);
+	TCCR0B = (0<<CS02)|(1<<CS01)|(0<<CS00);
 	TIMSK0 = (1<<OCIE0A);
 	
-	OCR0A = 156;
+	OCR0A = 249;
 	
 	sei();
 	
@@ -202,160 +161,194 @@ void init_pid(){ //Abilito t/c 0 per fare pid ogni intervallo di tempo (400 Hz, 
 
 ISR(INT0_vect){
 	
-	def_mot MOT;
+	int m=0;
 	
-	MOT.M[0].rs.temp = TCNT4;
+	RSP[m].temp = TCNT4;
 
 	if((PINH&0x08)!=0){
-		MOT.M[0].rs.dir = -1;
-	} else{
-		MOT.M[0].rs.dir = 1;
+		RSP[m].dir = -1;
+		} else{
+		RSP[m].dir = 1;
 	}
 	
-	MOT.M[0].rs.curr = MOT.M[0].rs.temp - MOT.M[0].rs.old;
-	MOT.M[0].rs.old = MOT.M[0].rs.temp;
+	if (RSP[m].temp > RSP[m].old)
+	RSP[m].curr = RSP[m].temp - RSP[m].old;
+
+	RSP[m].old = RSP[m].temp;
 
 }
 
 ISR(INT1_vect){
-
-	def_mot MOT;
 	
-	MOT.M[1].rs.temp = TCNT4;
+	int m=1;
+	
+	RSP[m].temp = TCNT4;
 
 	if((PINH&0x04)!=0){
-		MOT.M[1].rs.dir = -1;
+		RSP[m].dir = -1;
 		} else{
-		MOT.M[1].rs.dir = 1;
+		RSP[m].dir = 1;
 	}
 	
-	MOT.M[1].rs.curr = MOT.M[1].rs.temp - MOT.M[1].rs.old;
-	MOT.M[1].rs.old = MOT.M[1].rs.temp;
+	if (RSP[m].temp > RSP[m].old)
+	RSP[m].curr = RSP[m].temp - RSP[m].old;
 
+	RSP[m].old = RSP[m].temp;
 }
 
 ISR(INT2_vect){
-
-	def_mot MOT;
 	
-	MOT.M[2].rs.temp = TCNT4;
+	int m=3;
+	
+	RSP[m].temp = TCNT4;
 
 	if((PINH&0x01)!=0){
-		MOT.M[2].rs.dir = -1;
-	} else{
-		MOT.M[2].rs.dir = 1;
+		RSP[m].dir = -1;
+		} else{
+		RSP[m].dir = 1;
 	}
 	
-	MOT.M[2].rs.curr = MOT.M[2].rs.temp - MOT.M[2].rs.old;
-	MOT.M[2].rs.old = MOT.M[2].rs.temp;
+	if (RSP[m].temp > RSP[m].old)
+	RSP[m].curr = RSP[m].temp - RSP[m].old;
 
+	RSP[m].old = RSP[m].temp;
 }
 
 ISR(INT3_vect){
-
-	def_mot MOT;
 	
-	MOT.M[3].rs.temp = TCNT4;
+	int m=2;
+	
+	RSP[m].temp = TCNT4;
 
 	if((PINH&0x02)!=0){
-		MOT.M[3].rs.dir = -1;
-	} else{
-		MOT.M[3].rs.dir = 1;
+		RSP[m].dir = -1;
+		} else{
+		RSP[m].dir = 1;
 	}
 	
-	MOT.M[3].rs.curr = MOT.M[3].rs.temp - MOT.M[3].rs.old;
-	MOT.M[3].rs.old = MOT.M[3].rs.temp;
+	if (RSP[m].temp > RSP[m].old)
+	RSP[m].curr = RSP[m].temp - RSP[m].old;
 
+	RSP[m].old = RSP[m].temp;
 }
 
 ISR(TIMER0_COMPA_vect){
 	
-	def_mot MOT;
-	
-	pid_mot ++;
-	if(pid_mot >= 4)pid_mot = 0;
-	
-	MOT.M[pid_mot].sp.speed = (((double)(2000000)))/(((double)MOT.M[pid_mot].rs.curr) * 990);
-	
-	if (MOT.M[pid_mot].sp.speed < 3){
-		
-		Serial_Tx(MOT.M[pid_mot].sp.speed);
-		MOT.M[pid_mot].sp.wanted = 2;
-		
-		if (MOT.M[pid_mot].sp.speed < 150){
-
-			MOT.M[pid_mot].sp.err = MOT.M[pid_mot].sp.wanted - MOT.M[pid_mot].sp.speed;
-			
-			MOT.M[pid_mot].pid.P = MOT.M[pid_mot].sp.err * kp;
-			MOT.M[pid_mot].pid.I += MOT.M[pid_mot].sp.err * ki;
-			MOT.M[pid_mot].pid.D = (MOT.M[pid_mot].sp.err - MOT.M[pid_mot].sp.old_err) * ki;
-			
-			MOT.M[pid_mot].sp.old_err = MOT.M[pid_mot].sp.err;
-		
-			if(MOT.M[pid_mot].pid.P >= 500)MOT.M[pid_mot].pid.P = 500;
-			if(MOT.M[pid_mot].pid.P <= 0)MOT.M[pid_mot].pid.P = 0;
-		
-			if(MOT.M[pid_mot].pid.I >= 0.2)MOT.M[pid_mot].pid.I = 0.2;
-			if(MOT.M[pid_mot].pid.I <= 0)MOT.M[pid_mot].pid.I = 0;
-		
-			if(MOT.M[pid_mot].pid.D >= 200)MOT.M[pid_mot].pid.D = 200;
-			if(MOT.M[pid_mot].pid.D <= 0)MOT.M[pid_mot].pid.D = 0;
-
-			MOT.M[pid_mot].pid.cor = MOT.M[pid_mot].pid.P + MOT.M[pid_mot].pid.D + MOT.M[pid_mot].pid.I;
-		
-			if(MOT.M[pid_mot].pid.cor >= 1023)MOT.M[pid_mot].pid.cor = 1023;
-			if(MOT.M[pid_mot].pid.cor <= 0)MOT.M[pid_mot].pid.cor = 0;
-
-			MOT.M[pid_mot].dr.pwm = MOT.M[pid_mot].pid.cor;
-		
-		}
+	PiD.mot ++;
+	if(PiD.mot >= 4){
+		PiD.mot = 0;
+		setdrv = 1;
 	}
 	
-	if(pid_mot == 3){
-		driver_set();
+	speed[PiD.mot] = (((double)2000000.0) / (((double)RSP[PiD.mot].curr) * 990.0));
+	
+	int a;
+	a = speed[PiD.mot] * 100;
+	Serial_Tx(a);
+	
+	if (speed[PiD.mot] < 0.1){
+		speed[PiD.mot] = 0.0;
 	}
 	
+	if (speed[PiD.mot] < 3.0){
+		
+		wanted_speed[PiD.mot] = 2;
+		
+		PiD.P = 0;
+		PiD.D = 0;
+		PiD.correction = 0;
+		
+		if ((wanted_speed[PiD.mot] != 0) || ((wanted_speed[PiD.mot] == 0) && (speed[PiD.mot] != 0))){
+			
+			speed[PiD.mot] = speed[PiD.mot] * RSP[PiD.mot].dir;
+			speed_error = setspeed - speed[PiD.mot];
+			
+			PiD.P = speed_error * kp;
+			PiD.I[PiD.mot] += speed_error * ki;
+			PiD.D = (speed_error - old_error[PiD.mot]) * kd;
+			
+			old_error[PiD.mot] = speed_error;
+			
+			if(PiD.P > Plim) PiD.P = Plim;
+			if(PiD.P < -Plim) PiD.P = -Plim;
+			
+			if(PiD.I[PiD.mot] > Ilim) PiD.I[PiD.mot] = Ilim;
+			if(PiD.I[PiD.mot] < -Ilim) PiD.I[PiD.mot] = -Ilim;
+			
+			if(PiD.D > Dlim) PiD.D = Dlim;
+			if(PiD.D < -Dlim) PiD.D = -Dlim;
+			
+			PiD.correction = PiD.P + PiD.I[PiD.mot] + PiD.D;			// + DVR[PiD.mot].pwm
+			
+			if(PiD.correction > 1023)PiD.correction = 1023;
+			if(PiD.correction < -1023)PiD.correction = -1023;
+			
+			if(PiD.correction < 0){
+				PiD.correction *= -1;
+				DVR[PiD.mot].dir = -1;
+			}
+			
+			DVR[PiD.mot].pwm = PiD.correction;
+			
+			DIS.temp = (speed[PiD.mot] * diametro * M_PI) / 1000;
+			DIS.trav[PiD.mot] += DIS.temp;
+			
+		}else{
+			DVR[PiD.mot].pwm = 0;
+		}	
+	}
 }
 
-void driver_set(){
-	
-	def_mot MOT;
+int main(void){
 
-	if(MOT.M[0].dr.dir < 0){
-		PORTJ &= 0b111;
-		PORTJ |= 0b10000;
-	}else{
-		PORTJ &= 0b111;
-		PORTJ |= 0b1000;
+	init_gpio();
+	init_intt();
+	init_timer4();
+	init_pwm();
+	init_pid();
+	init_serial();
+
+	for(int i=0;i<4;i++){
+		PiD.I[i] = 0;
+		DVR[i].pwm = 0;
+		DVR[i].dir = 1;
 	}
 	
-	if(MOT.M[1].dr.dir < 0){
-		PORTJ &= 0b11100;
-		PORTJ |= 0b1;
-	}else{
-		PORTJ &= 0b11100;
-		PORTJ |= 0b10;
-	}
+	start_pwm();
 	
-	if(MOT.M[2].dr.dir < 0){
-		PORTA &= 0b11100000;
-		PORTA |= 0b01000;
-	}else{
-		PORTA &= 0b11100000;
-		PORTA |= 0b10000;
-	}
+	sei();
 	
-	if(MOT.M[3].dr.dir < 0){
-		PORTA &= 0b111000;
-		PORTA |= 0b10000000;
-	}else{
-		PORTA &= 0b111000;
-		PORTA |= 0b01000000;
+	while(1) {		
+		
+		if(setdrv == 1){
+			setdrv = 0;
+			OCR1B = DVR[0].pwm;
+			OCR1A = DVR[1].pwm;
+			OCR3B = DVR[2].pwm;
+			OCR3A = DVR[3].pwm;
+			
+			if(DVR[0].dir==1){
+				PORTJ &= 0b111;PORTJ |= 0b10000;
+				}else{
+				PORTJ &= 0b111;PORTJ |= 0b1000;
+			}
+			if(DVR[1].dir==1){
+				PORTJ &= 0b11100;PORTJ |= 0b1;
+				}else{
+				PORTJ &= 0b11100;PORTJ |= 0b10;
+			}
+			if(DVR[2].dir==1){
+				PORTA &= 0b11100000;PORTA |= 0b01000;
+				}else{
+				PORTA &= 0b11100000;PORTA |= 0b10000;
+			}
+			if(DVR[3].dir==1){
+				PORTA &= 0b111000;PORTA |= 0b10000000;
+				}else{
+				PORTA &= 0b111000;PORTA |= 0b01000000;
+			}
+		}
+		
 	}
-	
-	OCR1B = MOT.M[0].dr.pwm;
-	OCR1A = MOT.M[1].dr.pwm;
-	OCR3B = MOT.M[2].dr.pwm;
-	OCR3A = MOT.M[3].dr.pwm;
 
 }
