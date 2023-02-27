59a519ea5fe1e2d637268d0875f0c6e5bf39ca1f
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-02-27 02:11:16 +0100

    Update serial.c

--- Arduino/serial.c
+++ Arduino/serial.c
@@ -1,17 +1,12 @@
 #define F_CPU 16000000UL				//non necessaria
 #include <avr/io.h>					//necessaria per gestire PORT e PIN
-#define acc 255						//le memorie byte che rappresentano un uno logico sono a 255
 #include <avr/interrupt.h>				//necessaria per gestire sei()
 #include <util/delay.h>
 
-char byte;
+unsigned char byte;
 
 unsigned char USART_Receive( void )
 {
-	/* Wait for data to be received */
-	while ( !(UCSR0A & (1<<RXC0)) )
-	;
-	/* Get and return received data from buffer */
 	return UDR0;
 }
 
@@ -27,16 +22,21 @@ void Serial_Tx(int data) {
 int main(void)
 {
 	UBRR0H = 0;				//valore di UBRR0 che corrisponde alla baudrate 9600
-	UBRR0L = 0;				
-	UCSR0A = 0x00;				//RXC0 - TXC0 - UDRE0 - FE0 - DOR0 - UPE0 - U2X0 - MPCM0
-	UCSR0B = 0b11000;				//Abilitata Tx RXCIE0 - TXCIE0 - UDRIE0 - RXEN0 - TXEN0 - UCSZ10 - RXB80 - TXB80
+	UBRR0L = 0;
+	UCSR0A = 0x80;				//RXC0 - TXC0 - UDRE0 - FE0 - DOR0 - UPE0 - U2X0 - MPCM0
+	UCSR0B = 0x98;				//Abilitata Tx RXCIE0 - TXCIE0 - UDRIE0 - RXEN0 - TXEN0 - UCSZ10 - RXB80 - TXB80
 	UCSR0C = 0x06;				//Asincrono 8N1 UMSEL01 - UMSEL00 - UPM01 - UPM00 - USBS0 - UCSZ01 - UCSZ00 - UCPOL0
 
 
 	sei();					//abilitazione generale degli interrupt
 	while (1){
-		byte = USART_Receive();
+		
 		Serial_Tx(byte);
 		
-	}
+		}
+}
+
+ISR(USART0_RX_vect)
+{
+	byte = USART_Receive();
 }
