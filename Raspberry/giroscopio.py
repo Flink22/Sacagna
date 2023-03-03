562cafc68e987e9a22d7e4bb61823f41e1b50bac
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-03-03 03:49:21 +0100

    Update giroscopio.py

--- Raspberry/giroscopio.py
+++ Raspberry/giroscopio.py
@@ -1,6 +1,8 @@
 import smbus
 import time
 import struct
+import RPi.GPIO as gpio
+import board
 
 class BNO055:
     BNO055_ID 				= 0xA0
@@ -77,6 +79,8 @@ class BNO055:
 
 if __name__ == '__main__':
     bno = BNO055()
+    gpio.setup(10,gpio.OUT)
+    gpio.output(10,gpio.LOW)
     if bno.begin() is not True:
         print("Error initializing device")
         exit()
