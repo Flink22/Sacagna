1ae6b20684bacd5191b7e1f1d339900c16421f3f
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-03-03 03:49:00 +0100

    Update main.py

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -1,36 +1,171 @@
-import laser
 import RPi.GPIO as gpio
 import board
 import busio
 import math
 import time
-import Serial
+
+from laser import VL6180
+from giroscopio import BNO055
+from led import LED
+from telecamere import cameras
+from lettere import letters
+from seriale import SerialeATmega
+
+laser = VL6180()
+bno = BNO055()
+led = LED()
+cam = cameras()
+let = letters()
+ser = SerialeATmega()
 
 laser_MM = [0, 0, 0, 0, 0]
-led = [11, 25]
+muro = 120
+ang_attuale = 0
 
-# serial = Serial.SerialeATmega()
-laser = laser.Laser()
-
-# gpio.setup(led[0],gpio.OUT)
-# gpio.setup(led[1],gpio.OUT)
+if bno.begin() is not True:
+    print("Error initializing device")
+    exit()
 
 while True:
-#     letturaseriale = serial.read()
-    laser_MM = laser.read()
+#     cam.read()
+    laser_MM = laser.read(2)
     
-    ang_DX = (math.atan((laser_MM[1] - laser_MM[2])/170))*180/math.pi
-    ang_SX = (math.atan((laser_MM[4] - laser_MM[3])/170))*180/math.pi
+    if (((laser_MM[1] > muro) and (laser_MM[2] < muro)) or ((laser_MM[3] < muro) and (laser_MM[4] > muro))):
+        
+        ser.write(11)
+        time.sleep(0.1)
+        while((laser_MM[1] > muro) or (laser_MM[4] > muro)):
+            
+            laser.read()
+            print("trovo buco")
+        print("so fora")
+        ser.write(15)
+        data = 0
+        while(data < 205):
+            ser.clean()
+            time.sleep(0.25)
+#         if ser.read() != None:
+            data = ser.read()
+            print(data)
+        ser.write(10)
+        
+    elif (((laser_MM[1] < muro) and (laser_MM[2] > muro)) or ((laser_MM[3] < muro) and (laser_MM[4] < muro))):
+        
+        ser.write(11)
+        time.sleep(0.1)
+        while((laser_MM[2] > muro) or (laser_MM[3] > muro) ):
+            
+            laser.read()
+            print("passo buco")
+        ser.write(11)
+        data = 0
+        while(data < 205):
+            ser.clean()
+            time.sleep(0.25)
+#         if ser.read() != None:
+            data = ser.read()
+            print(data)
+        ser.write(10)
     
-    print(str(ang_DX) + " | " + str(ang_SX))
-#     print(letturaseriale)
-#     
-#     gpio.output(led[0],gpio.HIGH)
-#     gpio.output(led[1],gpio.HIGH)
-#     
-#     time.sleep(0.5)
-#     
-#     gpio.output(led[0],gpio.LOW)
-#     gpio.output(led[1],gpio.LOW)
-#     
-    time.sleep(0.5)
+    if ((laser_MM[1] < muro) and (laser_MM[2] < muro)):
+        ang_DX = (math.atan((laser_MM[1] - laser_MM[2])/175))*180/math.pi
+        
+        while (ang_DX > 3.0):
+            print("Raddrizzo")
+            laser_MM = laser.read()
+            ang_DX = (math.atan((laser_MM[1] - laser_MM[2])/175))*180/math.pi
+#             gira
+        
+        while (ang_DX < -3.0):
+            print("Raddrizzo")
+            laser_MM = laser.read()
+            ang_DX = (math.atan((laser_MM[1] - laser_MM[2])/175))*180/math.pi
+#             gira
+        
+    elif ((laser_MM[3] < muro) and (laser_MM[4] < muro)):
+        ang_SX = (math.atan((laser_MM[4] - laser_MM[3])/175))*180/math.pi
+        
+        while (ang_SX > 3.0):
+            print("Raddrizzo")
+            laser_MM = laser.read()
+            ang_SX = (math.atan((laser_MM[4] - laser_MM[3])/175))*180/math.pi
+#             gira
+        
+        while (ang_SX < -3.0):
+            print("Raddrizzo")
+            laser_MM = laser.read()
+            ang_SX = (math.atan((laser_MM[4] - laser_MM[3])/175))*180/math.pi
+#             gira
+    
+    if ((laser_MM[1] > muro) and (laser_MM[2] > muro)):
+        direzione = 2
+        ang_attuale += 90
+    elif laser_MM[0] > muro:
+        direzione = 1
+        ang_attuale += 0
+    elif ((laser_MM[3] > muro) and (laser_MM[4] > muro)):
+        direzione = 4
+        ang_attuale += 270
+    else:
+        direzione = 3
+        ang_attuale += 180
+    
+    if ang_attuale > 360:
+        ang_attuale -= 360
+    elif ang_attuale < 0:
+        ang_attuale += 360
+        
+    ser.write(10)
+    
+    bno.begin()
+    if direzione == 1:
+        print("Vado dritto")
+        bno_ANG = bno.readAngle()
+        time.sleep(0.5)
+    else:
+        bno_ANG = bno.readAngle()
+        if direzione == 2:
+            print("Ruoto di 90 gradi a destra")
+            while (bno_ANG < 85) or (bno_ANG > 350):
+                try:
+                    bno_ANG = bno.readAngle()
+                except:
+                    print("eeror")
+                time.sleep(0.25)
+                print(bno_ANG)
+                ser.write(12)
+            
+        elif direzione == 4:
+            print("Ruoto di 90 gradi a sinistra")
+            while (bno_ANG > 275) or (bno_ANG < 10):
+                try:
+                    bno_ANG = bno.readAngle()
+                except:
+                    print("eeror")
+                print(bno_ANG)
+                time.sleep(0.25)
+                ser.write(14)
+    
+        elif direzione == 3:
+            print("Ruoto di 180 gradi a destra")
+            while (bno_ANG < 175) or (bno_ANG > 350):
+                try:
+                    bno_ANG = bno.readAngle()
+                except:
+                    print("eeror")
+                print(bno_ANG)
+                time.sleep(0.25)
+                ser.write(12)
+        
+    ser.write(11)
+    time.sleep(0.1)
+    data = 0
+    while(data < 1370):
+        ser.clean()
+        time.sleep(0.25)
+#         if ser.read() != None:
+        data = ser.read()
+        print(data)
+    ser.write(10)
+    
+    time.sleep(1)
