46c7283ef34bd9145939a52b94ba3f7995e55449
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-04-08 11:32:23 +0200

    Update main.c

--- Pico/main.c
+++ Pico/main.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
 #include "hardware/irq.h"
+#include "hardware/clocks.h"
+
+#define PLL_SYS_KHZ (200 * 1000)
 
 #define UART_ID uart0
-#define BAUD_RATE 500000
+#define BAUD_RATE 1000000
 #define DATA_BITS 8
 #define STOP_BITS 1
 #define PARITY    UART_PARITY_NONE
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
+
+#define n_mot 4
+
+#define kp 50000.0
+#define ki 20000.0
+#define kd 0.0
+#define diametro 69.0
+
+#define Plim 25000.0
+#define Ilim 10000.0
+#define Dlim 0.0
 
 typedef struct {
     volatile uint IN1;
@@ -37,6 +52,15 @@ typedef struct {
 } def_readspeed;
 
 typedef struct {
+	int mot;
+	double P;
+	double I[n_mot];
+	double D;
+	double correction;
+    int pwm[n_mot];
+} def_pid;
+
+typedef struct {
     unsigned char M1;
     unsigned char M2;
     unsigned char M3;
@@ -44,55 +68,32 @@ typedef struct {
     unsigned char SERVO;
 } def_ser;
 
-def_driver DVR[4];
-def_readspeed RSP[4];
+def_driver DVR[n_mot];
+def_readspeed RSP[n_mot];
 def_ser SER;
+def_pid PiD;
 
 volatile int EN_MOT;
 volatile int SERVO_PIN_PWM;
 volatile int SERVO_PWM = 1250;
 volatile uint slice_ser;
-volatile unsigned char read;
+volatile char read;
 volatile int readcomplete;
 
+volatile double speed[n_mot] = {0};
+volatile double speed_error = 0;
+volatile double old_error[n_mot] = {0};
+volatile double wanted_speed[n_mot] = {0};
+volatile signed int wanted_dir[n_mot] = {0};
+
 void Serial_RX(void) {
-    unsigned char bytes[8] = {};
-    bytes[0] = uart_getc(uart0);
-
-    /*if(bytes[0] == 'S'){
-        for(int i=1;i<8;i++){
-            if(uart_is_readable(UART_ID)){
-                bytes[i] = uart_getc(uart0);
-            } else{
-                break;
-            }
-	    }
-
-        if(bytes[1] == 'M'){
-            SER.M1 = bytes[2];
-            SER.M2 = bytes[3];
-            SER.M3 = bytes[4];
-            SER.M4 = bytes[5];
-        }
-
-        if(bytes[6] == 'S'){
-            SER.SERVO = bytes[7];
-        } else{
-            SER.SERVO = 0;
-        }
-    }*/
-
-    read = bytes[0];
+    read = uart_getc(uart0);
     readcomplete = 1;
-
-	for(int i=0;i<4;i++){
-		RSP[i].impulsi = 0;
-	}
 }
 
 void Serial_TX(uint data){
     unsigned char temp[4];
-	
+	sleep_us(100);
 	for (int k=0; k<4; k++){
 		temp[k] = data%10;
 		data = data / 10;
@@ -102,8 +103,7 @@ void Serial_TX(uint data){
 		uart_putc(UART_ID, temp[k]);
 		sleep_us(1);
 	}
-
-    sleep_ms(1);
+    sleep_us(100);
 }
 
 void var_setup() {
@@ -147,10 +147,15 @@ void var_setup() {
     DVR[2].PWM_CHAN = 0;
     DVR[3].PWM_CHAN = 1;
 
-    RSP[0].curr = timer_hw->timelr;
-    RSP[1].curr = timer_hw->timelr;
-    RSP[2].curr = timer_hw->timelr;
-    RSP[3].curr = timer_hw->timelr;
+    PiD.I[0] = 0;
+    PiD.I[1] = 0;
+    PiD.I[2] = 0;
+    PiD.I[3] = 0;
+
+    RSP[0].curr = 50000;
+    RSP[1].curr = 50000;
+    RSP[2].curr = 50000;
+    RSP[3].curr = 50000;
 
     RSP[0].old = 0;
     RSP[1].old = 0;
@@ -166,12 +171,30 @@ void var_setup() {
     DVR[1].corr = 1;
     DVR[2].corr = -1;
     DVR[3].corr = -1;
-
 }
 
 int serv_getang(int ang) {
     int duty = ((ang / 18.0) * 125.0) + 1250;
     return duty;
+}
+
+void driver_set(int mot, int dir, int pwm) {
+    dir = dir * DVR[mot].corr;
+    if (dir == 1){
+        gpio_put(DVR[mot].IN1, 1);
+        gpio_put(DVR[mot].IN2, 0);
+    }else if (dir == -1){
+        gpio_put(DVR[mot].IN1, 0);
+        gpio_put(DVR[mot].IN2, 1);
+    }else if (dir == 0){ //BRAKE
+        pwm_set_chan_level(DVR[mot].slice, DVR[mot].PWM_CHAN, 0);
+        gpio_put(DVR[mot].IN1, 1);
+        gpio_put(DVR[mot].IN2, 1);
+    }else if (dir == 2){ //OFF
+        gpio_put(DVR[mot].IN1, 0);
+        gpio_put(DVR[mot].IN2, 0);
+    }
+    pwm_set_chan_level(DVR[mot].slice, DVR[mot].PWM_CHAN, pwm);
 }
 
 void mot_setup() {
@@ -215,12 +238,67 @@ void enc_interrupt(uint gpio, uint32_t events){
     if(gpio==RSP[2].ENC.IN1)m=2;
     if(gpio==RSP[3].ENC.IN1)m=3;
 
-    RSP[m].impulsi ++;
+    RSP[m].impulsi++;
+
+    RSP[m].temp = timer_hw->timelr;
+    RSP[m].curr = RSP[m].temp - RSP[m].old;
+    RSP[m].old = RSP[m].temp;
+}
+
+bool PID(struct repeating_timer *t) {
+
+    PiD.mot ++;
+	if(PiD.mot > 3){
+		PiD.mot = 0;
+	}
+	
+	speed[PiD.mot] = (((double)1000000.0) / (((double)RSP[PiD.mot].curr) * 990.0));
+
+    if (speed[PiD.mot] > 2.0)speed[PiD.mot] = 0.0;
+	
+	if (speed[PiD.mot] < 2.0){
+		
+		PiD.P = 0;
+		PiD.D = 0;
+		PiD.correction = 0;
+		
+		if (wanted_speed[PiD.mot]  != 0){
+			
+			speed_error = wanted_speed[PiD.mot] - speed[PiD.mot];
+			
+			PiD.P = speed_error * kp;
+			PiD.I[PiD.mot] += speed_error * ki;
+			
+			old_error[PiD.mot] = speed_error;
+			
+			if(PiD.P > Plim) PiD.P = Plim;
+			if(PiD.P < -Plim) PiD.P = -Plim;
+			if(PiD.I[PiD.mot] > Ilim) PiD.I[PiD.mot] = Ilim;
+			if(PiD.I[PiD.mot] < -Ilim) PiD.I[PiD.mot] = -Ilim;
+			
+			PiD.correction = PiD.P + PiD.I[PiD.mot];
+			
+			if(PiD.correction > 25000)PiD.correction = 25000;
+			if(PiD.correction < 0)PiD.correction = 0;
+
+            PiD.pwm[PiD.mot] = PiD.correction;
+
+            driver_set(PiD.mot, wanted_dir[PiD.mot], PiD.pwm[PiD.mot]);
+	    } else{
+            driver_set(PiD.mot, 0, 0);
+        }
+    }
+    return true;
 }
 
 void extint_setup() {
 	gpio_set_input_enabled(RSP[0].ENC.IN1, true);
     gpio_set_input_enabled(RSP[1].ENC.IN1, true);
+
+    gpio_pull_up(RSP[0].ENC.IN1);
+    gpio_pull_up(RSP[1].ENC.IN1);
+    gpio_pull_up(RSP[2].ENC.IN1);
+    gpio_pull_up(RSP[3].ENC.IN1);
     
     gpio_set_irq_enabled_with_callback(RSP[0].ENC.IN1, GPIO_IRQ_EDGE_RISE, true, &enc_interrupt);
     gpio_set_irq_enabled(RSP[1].ENC.IN1, GPIO_IRQ_EDGE_RISE, true);
@@ -229,7 +307,6 @@ void extint_setup() {
 }
 
 void uart_setup() {
-
     uart_init(UART_ID, 2400);
     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
@@ -244,27 +321,8 @@ void uart_setup() {
     uart_puts(UART_ID, "1");
 }
 
-void driver_set(int mot, int dir, int pwm) {
-    dir = dir * DVR[mot].corr;
-    if (dir == 1){
-        gpio_put(DVR[mot].IN1, 1);
-        gpio_put(DVR[mot].IN2, 0);
-    }else if (dir == -1){
-        gpio_put(DVR[mot].IN1, 0);
-        gpio_put(DVR[mot].IN2, 1);
-    }else if (dir == 0){ //BRAKE
-        pwm_set_chan_level(DVR[mot].slice, DVR[mot].PWM_CHAN, 0);
-        gpio_put(DVR[mot].IN1, 1);
-        gpio_put(DVR[mot].IN2, 1);
-    }else if (dir == 2){ //OFF
-        gpio_put(DVR[mot].IN1, 0);
-        gpio_put(DVR[mot].IN2, 0);
-    }
-    pwm_set_chan_level(DVR[mot].slice, DVR[mot].PWM_CHAN, pwm);
-}
-
 int main() {
-
+    set_sys_clock_khz(PLL_SYS_KHZ, true);
     stdio_init_all();
     var_setup();
     mot_setup();
@@ -274,46 +332,51 @@ int main() {
     SERVO_PWM = serv_getang(0);
     pwm_set_chan_level(slice_ser, PWM_CHAN_B, SERVO_PWM);
 
+    struct repeating_timer timer;
+    add_repeating_timer_us(-500, PID, NULL, &timer);
+
     while(1){
 
-        Serial_TX(RSP[1].impulsi);
+        Serial_TX(RSP[2].impulsi);
 
         if(readcomplete == 1){
-            readcomplete = 0;
 
+            readcomplete = 0;
             for(int i=0;i<4;i++){
                 RSP[i].impulsi = 0;
+                wanted_speed[i] = 0.8;
             }
 
             if(read == 11){
-                driver_set(3, 1, 17500);
-                driver_set(2, 1, 12500);
-                driver_set(1, 1, 12500);
-                driver_set(0, 1, 12500);
+                for(int i=0;i<4;i++){
+                    wanted_speed[i] = 1.0;
+                    wanted_dir[i] = 1;
+                }
             }else if(read == 12){
-                driver_set(0, -1, 12500);
-                driver_set(1, -1, 12500);
-                driver_set(2, 1, 12500);
-                driver_set(3, 1, 17500);
-            }else if(read == 15){
-                driver_set(3, -1, 17500);
-                driver_set(2, -1, 12500);
-                driver_set(1, -1, 12500);
-                driver_set(0, -1, 12500);
+                wanted_dir[0] = -1;
+                wanted_dir[1] = -1;
+                wanted_dir[2] = 1;
+                wanted_dir[3] = 1;
+            }else if(read == 13){
+                for(int i=0;i<4;i++){
+                    wanted_speed[i] = 0.8;
+                    wanted_dir[i] = -1;
+                }
             }else if(read == 14){
-                driver_set(0, 1, 12500);
-                driver_set(1, 1, 12500);
-                driver_set(2, -1, 12500);
-                driver_set(3, -1, 12500);
+                wanted_dir[0] = 1;
+                wanted_dir[1] = 1;
+                wanted_dir[2] = -1;
+                wanted_dir[3] = -1;
             }else if(read == 10){
                 for(int i=0;i<4;i++){
-                    RSP[i].impulsi = 0;
+                    wanted_speed[i] = 0.0;
+                    wanted_dir[i] = 0;
                 }
-                driver_set(0, 0, 0);
-                driver_set(1, 0, 0);
-                driver_set(2, 0, 0);
-                driver_set(3, 0, 0);
+            }
+            for(int i=0;i<4;i++){
+                RSP[i].impulsi = 0;
             }
         }
+
     }    
 }
