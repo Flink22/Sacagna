573fd57cf56c014ee5120938196fe66a5c3fd7f2
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-04-17 01:12:05 +0200

    Create lettere.py

---
+++ Raspberry/lettere.py
@@ -0,0 +1,54 @@
+import cv2 as cv
+import numpy as np
+
+class letters:
+    def __init__(self):
+        self.low_b = np.array([0, 0, 0])
+        self.high_b = np.array([20, 20, 20])
+        self.def_N = 0
+    
+    def find(self, blur, frame):
+        
+        ret,thresh = cv.threshold(blur, 100, 255, cv.THRESH_BINARY)
+        mask = cv.inRange(thresh, self.low_b, self.high_b)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # trova i contorni
+        self.def_N = -1
+        if len(contours) != 0:  
+            cnt = max(contours, key=cv.contourArea)
+            if cv.contourArea(cnt) > 1000:
+                hull = cv.convexHull(cnt, returnPoints = False)
+                try:
+                    defects = cv.convexityDefects(cnt, hull)
+                    count = 0
+                    if defects is not None:
+                        
+                        for i in range(defects.shape[0]):
+                            s,e,f,d = defects[i,0]
+                            start = tuple(cnt[s][0])
+                            end = tuple(cnt[e][0])
+                            far = tuple(cnt[f][0])
+                            dist = start[0] - end[0]
+                            if dist<0 :
+                                dist *= -1
+                            if dist>30 :
+                                count += 1
+                                cv.line(frame,start,end,[0,255,0],2)
+                                cv.circle(frame,far,5,[0,0,255],-1)
+                    
+                        if count == 0:
+                            print("S")
+                            self.def_N = 2
+                        elif count == 1:
+                            print("U")
+                            self.def_N = 0
+                        elif count == 2:
+                            print("H")
+                            self.def_N = 3
+                        else:
+                            print("NO LETTER")
+#                         cv.drawContours(frame, [hull], -1, (255, 255, 0), 2)
+                except:
+                    print("errore concavità")
+        else:
+            print("NO LETTER")
+        return mask, self.def_N
