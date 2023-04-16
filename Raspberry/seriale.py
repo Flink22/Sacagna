f3c3ee75d4d21bd85899080a5f1a96467307f162
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-04-16 23:27:51 +0200

    Update seriale.py

--- Raspberry/seriale.py
+++ Raspberry/seriale.py
@@ -1,69 +1,111 @@
 import serial
 import time
 import struct
-import RPi.GPIO as gpio
-import board
 
-class SerialeATmega:
+class SERIALEPICO:
     
     def __init__(self):
         self.ser = serial.Serial(
             port='/dev/ttyAMA0',
-            baudrate=500000,
+            baudrate = 1000000,
             parity= serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS, 
-            timeout = 0.001
+            timeout = 0
         )
-        self.error = "error"
-        self.noPort = "noPort"        
+        self.error = "serial error"
     
-    
-    def check_port(self):
-        if self.ser.isOpen() != True:
-            return self.noPort
-        else:
-            return 1
-    
-    
-    
-    def read(self, size = 4):
-        out = self.check_port()
-        if out == 1:
-            if(self.ser.in_waiting >= 0):
-                readbyte = self.ser.read(size)
-                if (len(readbyte) != size):
-                    out = self.error
+    def read(self, byte = 2):
+        start = time.time()
+        out = 0
+        while(self.ser.in_waiting <= 0):
+            if((time.time() - start) > 0.002):
+                print(self.error)
+                out = 1
+                break
+        if out != 1:
+            readbyte = self.ser.read(size = byte)
+            if (len(readbyte) != byte):
+                print(self.error)
+            else:
+                out = readbyte[0] + readbyte[1] * 100
+                if out != 1013:
+                    return out
                 else:
-                    out = readbyte[0] + readbyte[1] * 10 + readbyte[2] * 100 + readbyte[3] * 1000
-        else:
-            out = self.error
-        if out != "error":
-            return int(out)
-    
-    
+                    out = 5
+        return 1
     
     def write(self, byte):
-        out = self.check_port()
-        if out == 1:
-            self.ser.write(struct.pack('>B',byte))
+        self.ser.write(struct.pack('>B',byte))
+        
+    def vel(self, vel):
+        if vel > 1.55:
+            vel = 1.55
+        elif vel < 0.0:
+            vel = 0.0
+        return vel
     
+    def writeMot(self, vel_dx = 0.0, vel_sx = 0.0,  dir_dx = 0, dir_sx = 0):
+        #invia velocità motori, posso richiedere la distanza
+        #writeMot() ferma tutto e non richiede la distanza
+        
+        self.ser.flush()
+        
+        vel_dx_i = int((self.vel(vel_dx)) * 20)
+        vel_sx_i = int((self.vel(vel_sx)) * 20)
+        
+        dx = [0]*8
+        sx = [0]*8
+        
+        for i in range (1,6):
+            dx[i] = vel_dx_i % 2
+            sx[i] = vel_sx_i % 2
+            vel_dx_i = vel_dx_i // 2
+            vel_sx_i = vel_sx_i // 2
+            
+        byte = [0]*2
+        
+        byte[0] = (1<<7) | (1<<6) | (dir_dx<<0) #DX
+        byte[1] = (1<<7) | (0<<6) | (dir_sx<<0) #SX
+        for i in range (1,6):
+            byte[0] |= (dx[i]<<i)
+            byte[1] |= (sx[i]<<i)
+                
+        for i in range (2):
+            self.write(byte[i])
     
-    
+    def resetD(self):
+        self.clean()
+        byte = (0<<7) | (1<<6)
+        self.write(byte)
+        
+    def askD(self):
+        self.clean()
+        byte = (0<<7) | (0<<6) | (1<<5)
+        self.write(byte)
+        
     def clean(self):
         self.ser.flush()
         self.ser.flushInput()
 
 
 if __name__ == '__main__':
-    serial = SerialeATmega()
+    serial = SERIALEPICO()
+    serial.writeMot(1.0, 1.0, 1, 1)
+    time.sleep(1)
     while True:
-        data = serial.write(11)
-        while(data < 137):
-            data = serial.read()
-            print(data)
-            print('------')
-            serial.clean()
-            time.sleep(0.01)
-        time.sleep(2)
-        serial.clean()
+        a=0
+        while a<2850:
+            serial.writeMot(0.8, 0.8, 1, 1)
+            serial.askD()
+            print('--------')
+            a = serial.read()
+            print(a)
+        
+        serial.writeMot()
+        time.sleep(1)
+        serial.resetD()
+    
+    serial.writeMot()
+        
+
