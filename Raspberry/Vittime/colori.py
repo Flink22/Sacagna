dd144eefec47949168c7d8a51ee751002f753ef7
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-05-12 15:26:57 +0200

    Update colori.py

--- Raspberry/Vittime/colori.py
+++ Raspberry/Vittime/colori.py
@@ -1,54 +1,60 @@
 import cv2 as cv
 import numpy as np
 
-class letters:
+class colors:
     def __init__(self):
-        self.low_b = np.array([0, 0, 0])
-        self.high_b = np.array([20, 20, 20])
-        self.def_N = 0
-    
+        
+        self.areamin = 3000
+        
+        self.low_g = np.array([0, 200, 0])
+        self.high_g = np.array([0, 255, 0])
+        self.low_g2 = np.array([200, 200, 0])
+        self.high_g2 = np.array([255, 255, 0])
+        
+        self.low_y = np.array([0, 200, 200])
+        self.high_y = np.array([0, 255, 255])
+        
+        self.low_r = np.array([0, 0, 200])
+        self.high_r = np.array([0, 0, 255])
+        
+        self.col_N = 0
+        
     def find(self, blur, frame):
         
-        ret,thresh = cv.threshold(blur, 100, 255, cv.THRESH_BINARY)
-        mask = cv.inRange(thresh, self.low_b, self.high_b)
-        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # trova i contorni
-        self.def_N = -1
+        self.col_N = -1
+        
+        ret,thresh1 = cv.threshold(blur, 60, 255, cv.THRESH_BINARY)
+        mask = cv.inRange(thresh1, self.low_g, self.high_g)
+        mask2 = cv.inRange(thresh1, self.low_g2, self.high_g2)
+        mask += mask2
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
-            if cv.contourArea(cnt) > 1000:
-                hull = cv.convexHull(cnt, returnPoints = False)
-                try:
-                    defects = cv.convexityDefects(cnt, hull)
-                    count = 0
-                    if defects is not None:
-                        
-                        for i in range(defects.shape[0]):
-                            s,e,f,d = defects[i,0]
-                            start = tuple(cnt[s][0])
-                            end = tuple(cnt[e][0])
-                            far = tuple(cnt[f][0])
-                            dist = start[0] - end[0]
-                            if dist<0 :
-                                dist *= -1
-                            if dist>30 :
-                                count += 1
-                                cv.line(frame,start,end,[0,255,0],2)
-                                cv.circle(frame,far,5,[0,0,255],-1)
-                    
-                        if count == 0:
-                            print("S")
-                            self.def_N = 2
-                        elif count == 1:
-                            print("U")
-                            self.def_N = 0
-                        elif count == 2:
-                            print("H")
-                            self.def_N = 3
-                        else:
-                            print("NO LETTER")
-#                         cv.drawContours(frame, [hull], -1, (255, 255, 0), 2)
-                except:
-                    print("errore concavità")
-        else:
-            print("NO LETTER")
-        return mask, self.def_N
+            if cv.contourArea(cnt) > self.areamin:
+               print("VERDE")
+               self.col_N = 0
+               return thresh1, self.col_N
+        
+        ret,thresh2 = cv.threshold(blur, 130, 255, cv.THRESH_BINARY)
+        
+        mask = cv.inRange(thresh2, self.low_y, self.high_y)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) != 0:  
+            cnt = max(contours, key=cv.contourArea)
+            if cv.contourArea(cnt) > 5000:
+               print("GIALLO")
+               self.col_N = 1
+               return thresh2, self.col_N
+            
+        mask = cv.inRange(thresh2, self.low_r, self.high_r)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        if len(contours) != 0:  
+            cnt = max(contours, key=cv.contourArea)
+            if cv.contourArea(cnt) > self.areamin:
+               print("ROSSO")
+               self.col_N = 1
+               return thresh2, self.col_N
+        
+        if self.col_N == -1:
+            print("NO COLOR")
+        return thresh1, self.col_N
