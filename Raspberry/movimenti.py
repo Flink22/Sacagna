15ac3c7ee8606ca0d7db7b3e1e17188298b144f0
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-05-25 22:28:16 +0200

    Update movimenti.py

--- Raspberry/movimenti.py
+++ Raspberry/movimenti.py
@@ -1,4 +1,5 @@
 import time
+import math
 
 from seriale import SERIALEPICO
 from finecorsa import FINECORSA
@@ -15,24 +16,28 @@ class MOVIMENTI:
     def avanti(self, cm = 28.5):
         cm = cm * 100
         ser.resetD()
-        ser.writeMot(1.0, 1.0, 1, 1)
         
         Fc_avanti = fc.read()
         apds.begin()
+        bno.begin()
         
         dis = 0
         print("Avanti")
         while (dis < cm) and (Fc_avanti[1]) and (apds.readC()>50):
+            
+            #PID
+            ser.writeMot(1.0, 1.0, dx, sx)
+            
             ser.askD()
             dis = ser.read()
             time.sleep(0.002)
             Fc_avanti = fc.read()
-            print(dis)
             
         ser.writeMot()
-        time.sleep(0.25)
         ser.resetD()
         ser.clean()
+        if dis < 1000:
+            return 2
     
     def indietro(self, cm = 5.0):
         cm = cm * 100
@@ -42,13 +47,13 @@ class MOVIMENTI:
         dis = 0
         print("Indietro")
         while dis < cm:
+            ser.writeMot(1.0, 1.0, 0, 0)
             ser.askD()
             dis = ser.read()
             time.sleep(0.002)
             print(dis)
         
         ser.writeMot()
-        time.sleep(0.25)
         ser.resetD()
         ser.clean()
 
