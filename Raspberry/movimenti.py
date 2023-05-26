ec2bd1c0110d055570d5112235d77fa84ff5ddf8
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-05-26 13:44:30 +0200

    Update movimenti.py

--- Raspberry/movimenti.py
+++ Raspberry/movimenti.py
@@ -1,5 +1,4 @@
 import time
-import math
 
 from seriale import SERIALEPICO
 from finecorsa import FINECORSA
@@ -13,53 +12,63 @@ apds = APDS9960()
 
 class MOVIMENTI:
     
-    def avanti(self, cm = 28.5):
+    def avanti(self, cm = 28.5,nero = 750 ):
         cm = cm * 100
         ser.resetD()
+#         ser.writeMot(1.0, 1.0, 1, 1)
         
         Fc_avanti = fc.read()
         apds.begin()
-        bno.begin()
         
+        bno.begin()
+        rampa = 0
         dis = 0
         print("Avanti")
-        while (dis < cm) and (Fc_avanti[1]) and (apds.readC()>50):
-            
-            #PID
-            ser.writeMot(1.0, 1.0, dx, sx)
-            
+        while (dis < cm) and (Fc_avanti[1]) and (apds.readC()>nero):
+            ser.writeMot(1.5, 1.5, 1, 1)
+            if ((bno.readAngleInc() <= -15) or (bno.readAngleInc() >= 15)):
+                rampa = 1
+                while ((bno.readAngleInc() <= -15) or (bno.readAngleInc() >= 15)) and (Fc_avanti[1]):
+                    ser.writeMot(1.5, 1.5, 1, 1)
+                    Fc_avanti = fc.read()
+                    print("Rampa")
+                ser.resetD()
+                cm = 1300
             ser.askD()
             dis = ser.read()
-            time.sleep(0.002)
             Fc_avanti = fc.read()
             
+#         while ((bno.readAngleInc() <= -15) or (bno.readAngleInc() >= 15)) and (Fc_avanti[1]):
+#             ser.writeMot(1.0, 1.0, 1, 1)
+#             Fc_avanti = fc.read()
+#             print("Rampa")
+            
         ser.writeMot()
+        time.sleep(0.25)
         ser.resetD()
         ser.clean()
-        if dis < 1000:
-            return 2
     
-    def indietro(self, cm = 5.0):
+    def indietro(self, cm = 4.0):
         cm = cm * 100
         ser.resetD()
-        ser.writeMot(1.0, 1.0, 0, 0)
+        ser.writeMot(1.2, 1.2, 0, 0)
         
         dis = 0
         print("Indietro")
         while dis < cm:
-            ser.writeMot(1.0, 1.0, 0, 0)
+            ser.writeMot(1.2, 1.2, 0, 0) 
             ser.askD()
             dis = ser.read()
             time.sleep(0.002)
             print(dis)
         
         ser.writeMot()
+        time.sleep(0.25)
         ser.resetD()
         ser.clean()
 
 if __name__ == '__main__':
     mv = MOVIMENTI()
     while True:
-        mv.indietro()
+        mv.avanti(cm = 2000)
         time.sleep(5)
-
