3a6287b9ff5dbb3eae4e8fbfb1ef3ca82b0c107b
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-06-01 13:32:15 +0200

    Update colori.py

--- Raspberry/Vittime/colori.py
+++ Raspberry/Vittime/colori.py
@@ -6,55 +6,48 @@ class colors:
         
         self.areamin = 3000
         
-        self.low_g = np.array([0, 200, 0])
-        self.high_g = np.array([0, 255, 0])
-        self.low_g2 = np.array([200, 200, 0])
-        self.high_g2 = np.array([255, 255, 0])
+        self.low_g = np.array([30, 40, 40])
+        self.high_g = np.array([90, 255, 255])
         
-        self.low_y = np.array([0, 200, 200])
-        self.high_y = np.array([0, 255, 255])
+        self.low_y = np.array([20, 100, 100])
+        self.high_y = np.array([30, 255, 255])
         
-        self.low_r = np.array([0, 0, 200])
-        self.high_r = np.array([0, 0, 255])
+        self.low_r = np.array([0, 50, 50])
+        self.high_r = np.array([10, 255, 255])
         
-        self.col_N = 0
+        self.col_N = -1
         
     def find(self, blur, frame):
         
         self.col_N = -1
-        
-        ret,thresh1 = cv.threshold(blur, 60, 255, cv.THRESH_BINARY)
-        mask = cv.inRange(thresh1, self.low_g, self.high_g)
-        mask2 = cv.inRange(thresh1, self.low_g2, self.high_g2)
-        mask += mask2
+        hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
+        mask = cv.inRange(hsv, self.low_g, self.high_g)
         contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
             if cv.contourArea(cnt) > self.areamin:
                print("VERDE")
                self.col_N = 0
-               return thresh1, self.col_N
-        
-        ret,thresh2 = cv.threshold(blur, 130, 255, cv.THRESH_BINARY)
-        
-        mask = cv.inRange(thresh2, self.low_y, self.high_y)
+               return hsv, self.col_N
+                
+        mask = cv.inRange(hsv, self.low_y, self.high_y)
         contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
             if cv.contourArea(cnt) > 5000:
                print("GIALLO")
                self.col_N = 1
-               return thresh2, self.col_N
+               return hsv, self.col_N
             
-        mask = cv.inRange(thresh2, self.low_r, self.high_r)
+        mask = cv.inRange(hsv, self.low_r, self.high_r)
         contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
             if cv.contourArea(cnt) > self.areamin:
                print("ROSSO")
                self.col_N = 1
-               return thresh2, self.col_N
+               return hsv, self.col_N
         
         if self.col_N == -1:
             print("NO COLOR")
-        return thresh1, self.col_N
+        return hsv, self.col_N
