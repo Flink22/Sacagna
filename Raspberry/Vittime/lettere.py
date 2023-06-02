c0453f7db7efde48996e763a183a0ab31eb745dd
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-06-02 12:11:00 +0200

    Update lettere.py

--- Raspberry/Vittime/lettere.py
+++ Raspberry/Vittime/lettere.py
@@ -2,54 +2,54 @@ import cv2 as cv
 import numpy as np
 
 class letters:
-    def __init__(self):
-        self.def_N = -1
+    def __init__(self, nome):
+        self.nome = nome
+        self.low_b = 0
+        self.high_b = 20
+        self.def_N = 0
     
     def find(self, blur, frame):
         
         gray = cv.cvtColor(blur, cv.COLOR_BGR2GRAY)
-        thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 29, 19)
-        mask = cv.inRange(thresh, 0, 1)
-        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 29, 23)
+        mask = cv.inRange(thresh, self.low_b, self.high_b)
+        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE) # trova i contorni
         self.def_N = -1
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
-            if cv.contourArea(cnt) > 300:
+            if cv.contourArea(cnt) > 1200:
                 hull = cv.convexHull(cnt, returnPoints = False)
                 try:
                     defects = cv.convexityDefects(cnt, hull)
-                    countx = 0
-                    county = 0
+                    count = 0
                     if defects is not None:
+                        
                         for i in range(defects.shape[0]):
                             s,e,f,d = defects[i,0]
                             start = tuple(cnt[s][0])
                             end = tuple(cnt[e][0])
+                            far = tuple(cnt[f][0])
+                            dist = start[0] - end[0]
                             
-                            dx = abs(start[0] - end[0])
-                            dy = abs(start[1] - end[1])
-                            
-                            if dx > 25:
-                                countx += 1
-                            if dy > 50:
-                                county += 1
-                                
-                        if countx == 0:
-                            print("S")
+                            if abs(dist>30):
+                                count += 1
+                                cv.line(frame,start,end,[0,255,0],2)
+                                cv.circle(frame,far,5,[0,0,255],-1)
+                    
+                        if count == 0:
+                            print("Telecamera", self.nome, "S")
                             self.def_N = 1
-                        elif countx == 1:
-                            print("U")
+                        elif count == 1:
+                            print("Telecamera", self.nome, "U")
                             self.def_N = 0
-                        elif countx == 2:
-                            print("H")
+                        elif count == 2:
+                            print("Telecamera", self.nome, "H")
                             self.def_N = 3
                         else:
-                            print("NO LETTER")
+                            print("Telecamera", self.nome, "NO LETTER")
+#                         cv.drawContours(frame, [hull], -1, (255, 255, 0), 2)
                 except:
                     print("errore concavità")
-            else:
-                print("NO LETTER")
         else:
             print("NO LETTER")
         return mask, self.def_N
-
