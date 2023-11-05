46ba1f09bcae4a6d1588afdff8bf3511c5fbbcbc
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-11-05 12:13:33 +0100

    Update colori.py

--- Raspberry/Vittime/colori.py
+++ Raspberry/Vittime/colori.py
@@ -4,16 +4,18 @@ import numpy as np
 class colors:
     def __init__(self, nome):
         self.nome = nome
-        self.areamin = 1000
+        self.areamin = 1200
         
-        self.low_y = np.array([22, 60, 200])
-        self.high_y = np.array([60, 255, 255])
+        self.low_g = np.array([40, 90, 35])
+        self.high_g = np.array([100, 240, 200])
         
-        self.low_g = np.array([50, 70, 130])
-        self.high_g = np.array([90, 250, 255])
+        self.low_y = np.array([20, 100, 100])
+        self.high_y = np.array([30, 255, 255])
         
-        self.low_r = np.array([0, 100, 195])
-        self.high_r = np.array([8, 255, 255])
+        self.low_r = np.array([0, 50, 50])
+        self.high_r = np.array([10, 255, 255])
+        self.low_r2 = np.array([170, 50, 50])
+        self.high_r2 = np.array([180, 255, 255])
         
         self.col_N = -1
         
@@ -39,15 +41,16 @@ class colors:
                self.col_N = 1
                return hsv, self.col_N
             
-        mask = cv.inRange(hsv, self.low_r, self.high_r)
-        contours, hierarchy = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        rmask = cv.inRange(hsv, self.low_r, self.high_r)
+        rmask += cv.inRange(hsv, self.low_r2, self.high_r2)
+        contours, hierarchy = cv.findContours(rmask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:  
             cnt = max(contours, key=cv.contourArea)
             if cv.contourArea(cnt) > self.areamin:
                print("Telecamera", self.nome, "ROSSO")
                self.col_N = 1
-               return hsv, self.col_N
+               return rmask, self.col_N
         
-        if self.col_N == -1:
-            print("Telecamera", self.nome, "NO COLOR")
+#         if self.col_N == -1:
+#             print("Telecamera", self.nome, "NO COLOR")
         return hsv, self.col_N
