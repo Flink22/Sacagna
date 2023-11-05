b3f50a4f7f7a516e5d701d6717698373fa2b575b
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-11-05 12:10:02 +0100

    Update START.py

--- Raspberry/START.py
+++ Raspberry/START.py
@@ -9,12 +9,10 @@ import sys
 import RPi.GPIO as GPIO
 import time
 import cv2 as cv
+from colore import APDS9960
 
-# from oldmain import LOGICA
-import Main as main
-from telecamere import CAMERAS
 
-exposure = 100 # 100
+exposure = 20 # 100
 
 def testDevice(source):
    cap = cv.VideoCapture(source) 
@@ -22,40 +20,75 @@ def testDevice(source):
        print('Warning: unable to open video source: ', source)
 
 if __name__ == "__main__":
-        
+    
+    import Main as main
+    from telecamere import CAMERAS
+    
+#     for i in range (8):
+#         testDevice(i)
+    
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     
     _exit = multiprocessing.Event()
-#     
-#     for i in range (10):
-#         testDevice(i)
-    
-    capsx = cv.VideoCapture(6)
-    capdx = cv.VideoCapture(1)
+
+    capsx = 4
+    capdx = 0
     
     camsx = CAMERAS(exposure, capsx, 'SX')
     camdx = CAMERAS(exposure, capdx, 'DX')
-#     lg = LOGICA()
     
     camdx_q = multiprocessing.Queue(1)
     camsx_q = multiprocessing.Queue(1)
     vittima = multiprocessing.Queue(1)
     
-#     tel_sx = multiprocessing.Process(target=camsx.main, args=(camsx_q,vittima))
-#     tel_dx = multiprocessing.Process(target=camdx.main, args=(camdx_q,vittima))
+    tel_sx = multiprocessing.Process(target=camsx.main, args=(camsx_q,vittima))
+    tel_dx = multiprocessing.Process(target=camdx.main, args=(camdx_q,vittima))
+    
+    apds = APDS9960()
     
     try:
-#         tel_dx.start()
-#         tel_sx.start()
+        tel_dx.start()
+        tel_sx.start()
+        
+        apds.begin()
+        while(GPIO.input(22)):
+            print(apds.read())
+            time.sleep(0.05)
+        
+        out = main.main(camdx_q, camsx_q, vittima)
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
         
         while(GPIO.input(22)):
-            time.sleep(0.1)
+            time.sleep(0.05)
         
-        main.main(camdx_q, camsx_q, vittima)
+        out = main.main(camdx_q, camsx_q, vittima)
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        
+        while(GPIO.input(22)):
+            time.sleep(0.05)
+        
+        out = main.main(camdx_q, camsx_q, vittima)
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(22, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+        
+        while(GPIO.input(22)):
+            time.sleep(0.05)
+        
+        out = main.main(camdx_q, camsx_q, vittima)
         
         tel_dx.join()
         tel_sx.join()
+        print("FINE")
         
     except KeyboardInterrupt:
+        print("FINE")
+        tel_dx.close()
+        tel_sx.close()
         tel_dx.join()
         tel_sx.join()
