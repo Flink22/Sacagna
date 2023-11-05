fd04715ee301c3fb98fc3ea14a91de3c9ed4d919
Filippo Cracco <83571047+Flink22@users.noreply.github.com>
2023-11-05 12:16:06 +0100

    Update giroscopio.py

--- Raspberry/Librerie/giroscopio.py
+++ Raspberry/Librerie/giroscopio.py
@@ -5,94 +5,88 @@ import RPi.GPIO as gpio
 import board
 
 class BNO055:
-    BNO055_ID 				    = 0xA0
+    BNO055_ID 				= 0xA0
     
-    POWER_MODE_NORMAL 		    = 0X00
-    OPERATION_MODE_CONFIG 	    = 0X00
-    OPERATION_MODE_IMUPLUS 	    = 0X08
+    POWER_MODE_NORMAL 		= 0X00
+    OPERATION_MODE_CONFIG 	= 0X00
+    OPERATION_MODE_IMUPLUS 	= 0X08
     
-    VECTOR_EULER 			    = 0x1A
-    VECTOR_ACCELL               = 0x28
+    VECTOR_EULER 			= 0x1A
+    BNO055_LINEAR_ACCEL_DATA_X_LSB_ADDR = 0x28
     
-    BNO055_PAGE_ID_ADDR 	    = 0X07
-    BNO055_CHIP_ID_ADDR 	    = 0x00
+    BNO055_PAGE_ID_ADDR 	= 0X07
+    BNO055_CHIP_ID_ADDR 	= 0x00
     
-    BNO055_OPR_MODE_ADDR 	    = 0X3D
-    BNO055_PWR_MODE_ADDR 	    = 0X3E
+    BNO055_OPR_MODE_ADDR 	= 0X3D
+    BNO055_PWR_MODE_ADDR 	= 0X3E
     
-    BNO055_SYS_STAT_ADDR 	    = 0X39
-    BNO055_SYS_TRIGGER_ADDR     = 0X3F
-    BNO055_SELFTEST_RESULT_ADDR	= 0X36
+    BNO055_SYS_TRIGGER_ADDR = 0X3F
     
     def __init__(self, sensorId=-1, address=0x28):
+        gpio.setup(9,gpio.OUT)
+        gpio.output(9,gpio.LOW)
         gpio.setup(10,gpio.OUT)
         gpio.output(10,gpio.LOW)
+        gpio.output(9,gpio.HIGH)
+        time.sleep(0.5)
         self._sensorId = sensorId
         self._address = address
         self._mode = BNO055.OPERATION_MODE_IMUPLUS
     
     def begin(self, mode=None):
-        if mode is None:
-            mode = self._mode
+        if mode is None: mode = self._mode
         self._bus = smbus.SMBus(1)
-        
+
         if self.readBytes(BNO055.BNO055_CHIP_ID_ADDR)[0] != BNO055.BNO055_ID:
-            time.sleep(0.6)
+            time.sleep(0.8)# Wait for the device to boot up
             if self.readBytes(BNO055.BNO055_CHIP_ID_ADDR)[0] != BNO055.BNO055_ID:
                 return False
-        
+
         self.setMode(BNO055.OPERATION_MODE_CONFIG)
-        
+
+        # Trigger a reset and wait for the device to boot up again
         self.writeBytes(BNO055.BNO055_SYS_TRIGGER_ADDR, [0x20])
-        time.sleep(0.6)
+        time.sleep(0.8)
         while self.readBytes(BNO055.BNO055_CHIP_ID_ADDR)[0] != BNO055.BNO055_ID:
             time.sleep(0.01)
         time.sleep(0.05)
-        
+
+        # Set to normal power mode
         self.writeBytes(BNO055.BNO055_PWR_MODE_ADDR, [BNO055.POWER_MODE_NORMAL])
         time.sleep(0.01)
-        
+
         self.writeBytes(BNO055.BNO055_PAGE_ID_ADDR, [0])
         self.writeBytes(BNO055.BNO055_SYS_TRIGGER_ADDR, [0])
         time.sleep(0.01)
-        
-        self.setMode(BNO055.OPERATION_MODE_CONFIG)
-        time.sleep(0.025)
-        self.writeBytes(BNO055.BNO055_PAGE_ID_ADDR, [0])
-        self.writeBytes(BNO055.BNO055_SYS_TRIGGER_ADDR, [0x80])
+
         self.setMode(mode)
         time.sleep(0.02)
-        
+
         return True
-    
-    def setExternalCrystalUse(self, useExternalCrystal = True):
-	prevMode = self._mode
-	self.setMode(BNO055.OPERATION_MODE_CONFIG)
-	time.sleep(0.025)
-	self.writeBytes(BNO055.BNO055_PAGE_ID_ADDR, [0])
-	self.writeBytes(BNO055.BNO055_SYS_TRIGGER_ADDR, [0x80] if useExternalCrystal else [0])
-	time.sleep(0.01)
-	self.setMode(prevMode)
-	time.sleep(0.02)
-    
-    def getCalibration(self):
-	calData = self.readBytes(BNO055.BNO055_CALIB_STAT_ADDR)[0]
-	return (calData >> 6 & 0x03, calData >> 4 & 0x03, calData >> 2 & 0x03, calData & 0x03)
     
     def readAngleRot(self):
         buf = self.readBytes(BNO055.VECTOR_EULER, 6)
         xyz = struct.unpack('h', struct.pack('BB', buf[0], buf[1]))
-        return xyz[0]/16.0
+        out = xyz[0]/16.0
+        if out >= 360:
+            out = 360
+        elif out <= 0:
+            out = 0
+        return out
     
     def readAngleInc(self):
         buf = self.readBytes(BNO055.VECTOR_EULER, 6)
         xyz = struct.unpack('h', struct.pack('BB', buf[2], buf[3]))
-        return xyz[0]/16.0
+        out = xyz[0]/16.0
+        if out >= 180 or out <= -180:
+            out = 0
+        return out
     
     def sbatto(self):
         buf = self.readBytes(BNO055.BNO055_LINEAR_ACCEL_DATA_X_LSB_ADDR, 6)
         xyz = struct.unpack('h', struct.pack('BB', buf[0], buf[1]))
-        return xyz[0]/16.0
+        out = xyz[0]/16.0
+        return out
     
     def setMode(self, mode):
         self._mode = mode
@@ -108,12 +102,13 @@ class BNO055:
 
 if __name__ == '__main__':
     bno = BNO055()
-    gpio.setup(10,gpio.OUT)
-    gpio.output(10,gpio.LOW)
     if bno.begin() is not True:
         print("Error initializing device")
         exit()
     time.sleep(1)
     while True:
-        print(bno.sbatto())
+        ang = bno.readAngleInc()
+#         if ang < 0 or ang > 360:
+        print(ang)
         time.sleep(0.01)
+
